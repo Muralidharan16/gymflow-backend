37f81d65c54d279e9359a8b6f911f84a4472569a
Muralidharan16 <145135907+Muralidharan16@users.noreply.github.com>
2026-09-12 15:09:26 +0530

    P4E bracket clock-derived snapshot ages

--- tests/test_p4e_operational_snapshots_runtime.py
+++ tests/test_p4e_operational_snapshots_runtime.py
@@ -658,7 +658,7 @@ def test_maintenance_task_reads_exact_certified_aggregate_snapshots(
     from app.tasks import external_effect_observability as task_module
 
     monkeypatch.setattr(task_module.settings, "P4E_METRICS_OTLP_ENDPOINT", "")
-    expected_search = dict(
+    before_search = dict(
         zip(
             _SEARCH_COLUMNS,
             _snapshot(
@@ -669,7 +669,7 @@ def test_maintenance_task_reads_exact_certified_aggregate_snapshots(
             strict=True,
         )
     )
-    expected_refund = dict(
+    before_refund = dict(
         zip(
             _REFUND_COLUMNS,
             _snapshot(
@@ -683,7 +683,40 @@ def test_maintenance_task_reads_exact_certified_aggregate_snapshots(
 
     actual = asyncio.run(task_module._run_external_effect_operational_snapshot())
 
-    for key in _SEARCH_COLUMNS:
-        assert actual["search"][key] == expected_search[key]
-    for key in _REFUND_COLUMNS:
-        assert actual["refund"][key] == expected_refund[key]
+    after_search = dict(
+        zip(
+            _SEARCH_COLUMNS,
+            _snapshot(
+                _MAINTENANCE_LOGIN,
+                "MAINTENANCE_RUNTIME_PASSWORD",
+                _SEARCH_SNAPSHOT,
+            )[1],
+            strict=True,
+        )
+    )
+    after_refund = dict(
+        zip(
+            _REFUND_COLUMNS,
+            _snapshot(
+                _MAINTENANCE_LOGIN,
+                "MAINTENANCE_RUNTIME_PASSWORD",
+                _REFUND_SNAPSHOT,
+            )[1],
+            strict=True,
+        )
+    )
+
+    for key in _SEARCH_COLUMNS[:-1]:
+        assert actual["search"][key] == before_search[key] == after_search[key]
+    for key in _REFUND_COLUMNS[:-1]:
+        assert actual["refund"][key] == before_refund[key] == after_refund[key]
+    assert (
+        before_search["oldest_actionable_age_seconds"]
+        <= actual["search"]["oldest_actionable_age_seconds"]
+        <= after_search["oldest_actionable_age_seconds"]
+    )
+    assert (
+        before_refund["oldest_unresolved_age_seconds"]
+        <= actual["refund"]["oldest_unresolved_age_seconds"]
+        <= after_refund["oldest_unresolved_age_seconds"]
+    )
